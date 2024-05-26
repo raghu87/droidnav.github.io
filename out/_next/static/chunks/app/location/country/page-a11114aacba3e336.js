a044e37e69ba82e965fc76fc0e89dd6d84ea3dfb
Raghavendra Achar <raghuachar3187@gmail.com>
2024-05-26 18:17:56 +0530

    country added

---
+++ out/_next/static/chunks/app/location/country/page-a11114aacba3e336.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[49],{3014:function(n,e,u){Promise.resolve().then(u.bind(u,1238))}},function(n){n.O(0,[515,544,238,971,23,744],function(){return n(n.s=3014)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/app/technology/page-1cce17fe0f868bf7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[795],{4348:function(n,e,i){Promise.resolve().then(i.t.bind(i,3749,23)),Promise.resolve().then(i.bind(i,1238))},3749:function(){}},function(n){n.O(0,[601,515,544,238,971,23,744],function(){return n(n.s=4348)}),_N_E=n.O()}]);
