7a3d1e9eb9287cae9ba0132d2b836169e32d86aa
Raghavendra Achar <raghuachar3187@gmail.com>
2024-12-29 22:51:25 +0530

    states desc added

---
+++ out/_next/static/chunks/app/historical/page-df12a6ba9a21fc96.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2721],{915:function(n,e,i){Promise.resolve().then(i.t.bind(i,1817,23)),Promise.resolve().then(i.bind(i,6092))},1817:function(){}},function(n){n.O(0,[1020,1606,6544,6092,2971,7023,1744],function(){return n(n.s=915)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/app/location/country/page-060cb2cf625f3dce.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[49],{2489:function(n,e,u){Promise.resolve().then(u.bind(u,6092))}},function(n){n.O(0,[1606,6544,6092,2971,7023,1744],function(){return n(n.s=2489)}),_N_E=n.O()}]);
