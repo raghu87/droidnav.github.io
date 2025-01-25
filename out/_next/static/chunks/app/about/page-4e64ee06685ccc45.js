551b6296f52b60b94f742e5a76921e8e6d14bf7b
Raghavendra Achar <raghuachar3187@gmail.com>
2025-01-25 17:16:06 +0530

    new changes

---
+++ out/_next/static/chunks/app/about/page-4e64ee06685ccc45.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7301],{9248:function(n,e,i){Promise.resolve().then(i.t.bind(i,4064,23)),Promise.resolve().then(i.bind(i,2040))},4064:function(){}},function(n){n.O(0,[4237,6544,2040,2971,7023,1744],function(){return n(n.s=9248)}),_N_E=n.O()}]);
---
+++ out/_next/static/chunks/app/location/country/page-773d409b9abd20d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[49],{2073:function(n,e,u){Promise.resolve().then(u.bind(u,2040))}},function(n){n.O(0,[4237,6544,2040,2971,7023,1744],function(){return n(n.s=2073)}),_N_E=n.O()}]);
